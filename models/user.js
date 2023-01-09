db9da8334f6895d07bd86a0357adb4a5608219e4
Evan Fetherolf <efether234@gmail.com>
2023-01-08 18:47:46 -0800

    Create User model.

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.methods.generateAuthToken = function () {
+    return jwt.sign({
+        _id: this._id
+    },
+    'privatekey')
+}
+
+module.exports.User = new mongoose.model('User', userSchema)
